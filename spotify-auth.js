78340a0a1e259985deebd60a2e2aa70d7458a86e
aryan-rastogi <75449029+aryan-rastogi@users.noreply.github.com>
2021-01-17 22:15:03 -0500

    Update spotify-auth.js

--- spotify-auth.js
+++ spotify-auth.js
@@ -4,7 +4,7 @@ function authSpotify() {
 
   const authEndpoint = "https://accounts.spotify.com/authorize";
   const clientId = "2c059958c8a548a69d95f751753097b6";
-  const redirectUri = "https://damanjitg.github.io/weathervibe/callback.html";
+  const redirectUri = "https://aryan-rastogi.github.io/weathervibe/callback.html";
   const scopes = ["playlist-modify-public","playlist-modify-private","user-read-private"];
 
   window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token`
